d6594b63149d4773a585628d89f30357204e1825
ruikdev <166296471+ruikdev@users.noreply.github.com>
2024-09-11 13:54:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,87 @@
-import requests
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import urllib.request
 import json
+import os
+import time
+from collections import OrderedDict
+from bs4 import BeautifulSoup
+import requests
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import messagebox, ttk
+
+# Url base des directions
+dir_url = 'http://www.t2c.fr/admin/synthese?SERVICE=page&p=17732927961956390&noline='
+# Url base des arrêts
+stop_url = 'http://www.t2c.fr/admin/synthese?SERVICE=page&p=17732927961956392&numeroroute='
+
+lines = {
+    'A': '11821953316814895',
+    'B': '11821953316814897',
+    'C': '11821953316814915',
+    '3': '11821953316814882',
+    '4': '11821953316814888',
+    '5': '11821953316814889',
+    '7': '11821953316814891',
+    '8': '11821953316814892',
+    '9': '11821953316814893',
+    '10': '11821953316814874',
+    '12': '11821953316814875',
+    '13': '11821953316814876',
+    '20': '11821953316814877',
+    '21': '11821953316814878',
+    '22': '11821953316814879',
+    '23': '11822086460801028',
+    '24': '11821953316814913',
+    '25': '11822086460801025',
+    '26': '11821953316814880',
+    '27': '11821953316814881',
+    '28': '11822086460801030',
+    '31': '11821953316814883',
+    '32': '11821953316814884',
+    '33': '11821953316814914',
+    '34': '11821953316814885',
+    '35': '11821953316814886',
+    '36': '11821953316814887',
+    '37': '11822086460801032'
+}
+
+
+def get_line_data(url):
+    item_list = OrderedDict()
+    req = urllib.request.urlopen(url)
+    soup = BeautifulSoup(req, from_encoding='utf-8', features='html.parser')
+
+    for item in soup.find_all('option')[1:]:
+        item_name = item.text.strip()
+        item_num = item['value']
+        item_list[item_name] = item_num
+
+    return item_list
+
+
+def fill_json():
+    data = {'lines': []}
+
+    for line_name, line_num in lines.items():
+        print(f'Traitement de la ligne {line_name}')
+        line_data = {'line_name': line_name, 'line_num': line_num, 'directions': []}
+
+        line_dir = get_line_data(dir_url + line_num)
+        for dir_name, dir_num in line_dir.items():
+            dir_data = {'dir_name': dir_name, 'dir_num': dir_num, 'stops': []}
+
+            line_stop = get_line_data(stop_url + dir_num)
+            for stop_name, stop_num in line_stop.items():
+                dir_data['stops'].append({'stop_name': stop_name, 'stop_num': stop_num})
+
+            line_data['directions'].append(dir_data)
+
+        data['lines'].append(line_data)
+
+    with open('t2c_data.json', 'w', encoding='utf-8') as json_file:
+        json.dump(data, json_file, ensure_ascii=False, indent=4)
 
 
 def test_api(stop_id):
@@ -65,15 +145,31 @@ def horaire_bus():
 
 
 def afficher_id_arrets():
-    # Cette fonction pourrait être implémentée pour afficher une liste des ID d'arrêts
-    # Pour l'instant, elle affiche juste un message
-    messagebox.showinfo("ID des arrêts", "Fonctionnalité non implémentée")
+    with open('t2c_data.json', 'r', encoding='utf-8') as json_file:
+        data = json.load(json_file)
+
+    fenetre_arrets = tk.Toplevel()
+    fenetre_arrets.title("ID des arrêts")
+    fenetre_arrets.geometry("500x400")
+
+    tree = ttk.Treeview(fenetre_arrets, columns=('Ligne', 'Direction', 'Arrêt', 'ID'), show='headings')
+    tree.heading('Ligne', text='Ligne')
+    tree.heading('Direction', text='Direction')
+    tree.heading('Arrêt', text='Arrêt')
+    tree.heading('ID', text='ID')
+    tree.pack(expand=True, fill=tk.BOTH)
+
+    for line in data['lines']:
+        for direction in line['directions']:
+            for stop in direction['stops']:
+                tree.insert('', 'end',
+                            values=(line['line_name'], direction['dir_name'], stop['stop_name'], stop['stop_num']))
 
 
 def main():
     fenetre_menu = tk.Tk()
     fenetre_menu.title("T2C Bus")
-    fenetre_menu.geometry("200x150")
+    fenetre_menu.geometry("200x200")
 
     button_horaire = tk.Button(fenetre_menu, text="Prochain bus à l'arrêt", command=horaire_bus)
     button_horaire.pack(pady=5)
@@ -81,8 +177,13 @@ def main():
     button_arret = tk.Button(fenetre_menu, text="ID des arrêts", command=afficher_id_arrets)
     button_arret.pack(pady=5)
 
+    button_update = tk.Button(fenetre_menu, text="Mettre à jour les données", command=fill_json)
+    button_update.pack(pady=5)
+
     fenetre_menu.mainloop()
 
 
 if __name__ == "__main__":
+    if not os.path.exists('t2c_data.json'):
+        fill_json()
     main()
